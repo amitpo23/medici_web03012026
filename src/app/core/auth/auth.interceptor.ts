e1a58411145342d0c11988b8d7bd3c9756ea447f
Liashenko Vitaliy <vitaliy.liashenko@smartteksas.com>
2024-04-18 17:54:18 +0300

    Fix cors header error on prod

--- src/app/core/auth/auth.interceptor.ts
+++ src/app/core/auth/auth.interceptor.ts
@@ -1,63 +1,43 @@
-import { Injectable } from '@angular/core';
-import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
-import { AuthService } from './auth.service';
+import {Injectable} from '@angular/core';
+import {HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {Observable, throwError} from 'rxjs';
+import {catchError} from 'rxjs/operators';
+import {AuthService} from './auth.service';
 
 @Injectable()
-export class AuthInterceptor implements HttpInterceptor
-{
-    /**
-     * Constructor
-     */
-    constructor(private _authService: AuthService)
-    {
+export class AuthInterceptor implements HttpInterceptor {
+  constructor(private _authService: AuthService) {}
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    let newReq = req.clone();
+
+    // Request
+    //
+    // If the access token didn't expire, add the Authorization header.
+    // We won't add the Authorization header if the access token expired.
+    // This will force the server to return a "401 Unauthorized" response
+    // for the protected API routes which our response interceptor will
+    // catch and delete the access token from the local storage while logging
+    // the user out from the app!
+    let currentToken = localStorage.getItem('auth');
+    if (currentToken != null && currentToken != 'undefined') {
+      newReq = req.clone({
+        setHeaders: {
+          'Authorization': 'Bearer ' + currentToken,
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': 'true'
+        }
+      });
     }
+    return next.handle(newReq).pipe(
+      catchError((error) => {
 
-    /**
-     * Intercept
-     *
-     * @param req
-     * @param next
-     */
-    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>>
-    {
-        // Clone the request object
-        let newReq = req.clone();
-
-        // Request
-        //
-        // If the access token didn't expire, add the Authorization header.
-        // We won't add the Authorization header if the access token expired.
-        // This will force the server to return a "401 Unauthorized" response
-        // for the protected API routes which our response interceptor will
-        // catch and delete the access token from the local storage while logging
-        // the user out from the app!
-        let currentToken = localStorage.getItem('auth');   
-        // if ( this._authService.accessToken && !AuthUtils.isTokenExpired(this._authService.accessToken) )
-        if (currentToken != null && currentToken != 'undefined')
-        {   
-            newReq = req.clone({
-                headers: req.headers.set('Authorization', 'Bearer ' + currentToken) // this._authService.accessToken)
-            });
+        if (error instanceof HttpErrorResponse && (error.status === 401 || error.status === 400)) {
+          this._authService.signOut();
         }
 
-        // Response
-        return next.handle(newReq).pipe(
-            catchError((error) => {
-
-                // Catch "401 Unauthorized" responses
-                if ( error instanceof HttpErrorResponse && (error.status === 401 || error.status === 400))
-                {
-                    // Sign out
-                    this._authService.signOut();
-
-                    // Reload the app
-                    //location.reload();
-                }
-
-                return throwError(error);
-            })
-        );
-    }
+        return throwError(error);
+      })
+    );
+  }
 }
